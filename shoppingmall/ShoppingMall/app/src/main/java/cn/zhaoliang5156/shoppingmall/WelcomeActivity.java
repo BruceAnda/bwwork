6fc672310375d50e77e4d1ae73845f6ece0cb0ef
zhaoliang <2668645098@qq.com>
2019-05-12 18:10:17 +0800

    first commit

---
+++ shoppingmall/ShoppingMall/app/src/main/java/cn/zhaoliang5156/shoppingmall/WelcomeActivity.java
@@ -0,0 +1,13 @@
+package cn.zhaoliang5156.shoppingmall;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
